a72d80c2f2207e6fb777b53c63cf9eab0ff3feff
Christopher McGinnis <cmcginnis@elevatedsoftware.com>
2014-04-17 07:24:38 -0700

    base project and classes

---
+++ projects/Elevated.FileCache/CacheController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevated.FileCache
+{
+	public class CacheController
+	{
+
+	}
+}
